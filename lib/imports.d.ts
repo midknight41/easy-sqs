fc520eb70f341ba6e332713e452a7867cd1638e4
midknight41 <chad_macey@hotmail.com>
2014-07-29 09:48:51 +0100

    fixed dependency in package.json

--- lib/imports.d.ts
+++ lib/imports.d.ts
@@ -1,9 +1,5 @@
-/// <reference path="C:\Projects\typescript-node-definitions\node.d.ts" />
-/// <reference path="C:\Projects\typescript-node-definitions\request.d.ts" />
-/// <reference path="C:\Projects\typescript-node-definitions\nconf.d.ts" />
-/// <reference path="C:\Projects\typescript-node-definitions\gently.d.ts" />
-/// <reference path="C:\Projects\typescript-node-definitions\aws-sdk.d.ts" />
-/// <reference path="C:\Projects\textteaser.js\TextTeaser\lib\index.ts" />
+///<reference path="C:\Projects\typescript-node-definitions\node.d.ts" />
+///<reference path="C:\Projects\typescript-node-definitions\gently.d.ts" />
+///<reference path="C:\Projects\typescript-node-definitions\aws-sdk.d.ts" />
 ///<reference path="C:\Projects\typescript-node-definitions\nodeunit.d.ts"/>
 ///<reference path="C:\Projects\typescript-node-definitions\nock.d.ts"/>
-///<reference path="C:\Projects\typescript-node-definitions\async.d.ts"/>
